e54830193581eb12fd47446d2ed1c84cead7ad08
rafaela-soares-a <raffasoares95@gmail.com>
2023-02-17 20:51:10 -0300

    atividade-realizada

---
+++ src/controller/UserController.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from "express";
+import { UserBusiness } from "../business/UserBusiness"
+import { SignupInputDTO } from "../dtos/userDTO";
+import { BaseError } from "../errors/BaseError";
+
+
+
+export class UserController {
+    constructor (
+        private userBusiness: UserBusiness
+    ) {}
+
+    public signup = (req: Request, res: Response) => {
+        try {
+            const input: SignupInputDTO = {
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password
+            }
+
+            const output = this.userBusiness.signup(input)
+
+            res.status(201).send(output)
+            
+        } catch (error) {
+            if(error instanceof BaseError){
+                res.status(error.statusCode).send(error.message)
+            } else {
+                res.status(500).send("Erro inesperado")
+            }
+        }
+
+    }
+}
